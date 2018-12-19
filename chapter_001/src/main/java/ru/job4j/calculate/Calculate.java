a1dacf5193754e6ae3c2bc897fc097b9b03a2297
Sergo Zurabishvili <Kekelidze78@gmail.com>
2018-12-19 01:43:42 -0800

    1.3. JavaDoc. Code convention[#89832]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,15 @@
 package ru.job4j.calculate;
-
+/**
+ * Calculate
+ * 
+ * @author Sergo Zurabishvili (Kekelidze78@gmail.com)
+ */
 public class Calculate {
 	
+	/**
+	 * Main
+	 * @param args - args
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task.
+ *
+ *  @author Sergo Zurabishvili (Kekelidze78@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */

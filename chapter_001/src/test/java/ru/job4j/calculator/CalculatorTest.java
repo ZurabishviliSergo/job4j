4c2342b06948f4f0a3f2f19c08afa7b56d13e91f
Sergo Zurabishvili <Kekelidze78@gmail.com>
2019-01-06 04:34:45 -0800

    3.1. Элементарный калькулятор.[#89823]

--- chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
+++ chapter_001/src/test/java/ru/job4j/calculator/CalculatorTest.java
@@ -19,4 +19,39 @@ public class CalculatorTest  {
         double expected = 2D;
         assertThat(result, is(expected));
     }
+
+    /**
+     * test subtract
+     */
+    @Test
+    public void whenAddThreeMinusOneEqualsTwo()  {
+        Calculator calc = new Calculator();
+        calc.add(3D, 1D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
+    /**
+     * test multiply
+     */
+    @Test
+    public void whenAddThreeMultiplyTwoEqualsSix(){
+        Calculator calc =new Calculator();
+        calc.add(3D, 2D);
+        double result = calc.getResult();
+        double expected = 6D;
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * test divided
+     */
+    @Test
+    public void whenAddFourDividedTwoEqualsTwo(){
+        Calculator calc = new Calculator();
+        calc.add(4D, 2D);
+        double result = calc.getResult();
+        double expected = 2D;
+        assertThat(result, is(expected));
+    }
 }

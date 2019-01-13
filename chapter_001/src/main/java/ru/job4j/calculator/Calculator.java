ee7fc6e72d9d5d9083b72cb2c1685317258ddcba
Sergo Zurabishvili <Kekelidze78@gmail.com>
2019-01-12 22:32:25 -0800

    3.1. Элементарный калькулятор.[#89823]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -13,8 +13,35 @@ public class Calculator {
     public double getResult(){
         return this.result;
     }
-    public void add(double first, double second) {
 
+    /**
+     * method subtract
+     * @param first
+     * @param second
+     */
+    public void subt(double first, double second) {
+        this.result = first - second;
+    }
+    public double subt() {
+        return this.result;
+    }
+    /**
+     * method multiply
+     */
+    public void mult(double first, double second) {
+        this.result = first * second;
+    }
+    public double mult() {
+        return this.result;
+    }
+    /**
+     * method divided
+     */
+    public void divd(double first, double second){
+        this.result = first / second;
+    }
+    public double divd(){
+        return this.result;
     }
 
 }

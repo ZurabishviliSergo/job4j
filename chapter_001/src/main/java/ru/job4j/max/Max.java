6abeee86097adb2f1c508f69331638caf25646c0
Sergo Zurabishvili <Kekelidze78@gmail.com>
2019-01-23 17:45:43 -0800

    Максимум из трех чисел

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -12,4 +12,8 @@ public class Max {
     public int max(int first, int second) {
         return first > second ? first : second;
     }
+    //Максимум из трех чисел
+    public int max(int first, int second, int third) {
+        return this.max(this.max(first, second), third);
+    }
 }

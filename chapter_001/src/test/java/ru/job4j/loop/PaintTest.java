845be38e8bde2cfa7ee48c42d127424d3de5ac04
Sergo Zurabishvili <Kekelidze78@gmail.com>
2019-02-07 05:18:16 -0800

    Ппостроение пирамиды

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+ import org.junit.Test;
+ import java.util.StringJoiner;
+ import static org.hamcrest.Matchers.is;
+ import static org.junit.Assert.*;
+/**
+ * @author Sergo Zurabishvili(Kekelidze78@gmail.com).
+ * @since 7.02.2019.
+ *
+ */
+public class PaintTest {
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+}

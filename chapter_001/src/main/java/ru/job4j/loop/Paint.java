cf65f2f9b49b80389c47a0871395af90c969f129
Sergo Zurabishvili <Kekelidze78@gmail.com>
2019-02-07 05:40:03 -0800

    Построение пирамиды

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -44,4 +44,19 @@ public class Paint {
         }
         return screen.toString();
     }
+    public String pyramid(int height) {
+        StringBuilder screen = new StringBuilder();
+        int weight = 2 * height - 1;
+        for (int row = 0; row != height; row++) {
+            for (int column = 0; column != weight; column++) {
+                if (row >= height - column - 1 && row + height - 1 >= column) {
+                    screen.append("^");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(System.lineSeparator());
+        }
+        return screen.toString();
+    }
 }

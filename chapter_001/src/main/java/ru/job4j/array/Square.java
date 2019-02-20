8f02efca7cca5c91b60da2bf99d3ea775b9b22dc
Sergo Zurabishvili <Kekelidze78@gmail.com>
2019-02-19 23:11:03 -0800

    Заполнение массив степенями чисел

---
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+/**
+ * @author Sergo Zurabishvili (Kekelidze78@gmail.com).
+ * @since 18.02.2019.
+ */
+public class Square {
+public int[] calculate(int bound) {
+    int[] rst = new int[bound];
+    for(int j = 0; j <= 1; j++) {
+        rst[j - 1] = j * j;
+    }
+    return rst;
+}
+}

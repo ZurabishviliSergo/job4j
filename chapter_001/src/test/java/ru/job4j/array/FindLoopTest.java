606e31764b33d2d8083d1518b4dd4d92a2b00d00
Sergo Zurabishvili <Kekelidze78@gmail.com>
2019-02-21 12:02:05 -0800

    Классический поиск перебором

---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+/**
+ * @author Sergo zurabishvili (Kekelidze78@gmail.com).
+ * @since 21.02.2019
+ *
+ */
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/*
+*Test FindLoop
+ */
+public class FindLoopTest {
+    @Test
+    public void whenArrayHasLenghThen0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {
+                5, 10, 3
+        };
+        int value = 5;
+        int result = find.index(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+}

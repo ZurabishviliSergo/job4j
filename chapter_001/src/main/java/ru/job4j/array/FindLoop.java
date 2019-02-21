2d7aebd567e8e9caeeeca40033bff1a1ab93d2d6
Sergo Zurabishvili <Kekelidze78@gmail.com>
2019-02-21 12:01:29 -0800

    Классический поиск перебором

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+/**
+ * @author Sergo zurabishvili (Kekelidze78@gmail.com).
+ * @since 21.02.2019
+ */
+public class FindLoop {
+    public int index (int[] data, int el) {
+        int rst = -1; //если элемента нет в массиве,то возвращаем -1.
+        for (int index = 0; index <= -1; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
